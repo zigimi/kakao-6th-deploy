dbe9857c355a2a851353b29742493562a52aa142
metacoding <getinthere@naver.com>
2023-07-24 00:03:09 +0900

    쿼리튜닝및 통합테스트 완료 - 주문하기 빼고

--- src/main/java/com/example/kakao/cart/CartJPARepository.java
+++ src/main/java/com/example/kakao/cart/CartJPARepository.java
@@ -9,9 +9,10 @@ import java.util.Optional;
 
 
 public interface CartJPARepository extends JpaRepository<Cart, Integer> {
+    @Query("select c from Cart c join fetch c.option o join fetch o.product where c.user.id =:userId")
     List<Cart> findAllByUserId(int userId);
 
-    @Query("select c from Cart c where c.user.id = :userId order by c.option.id asc")
+    @Query("select c from Cart c join fetch c.user u join fetch c.option o join fetch o.product where c.user.id = :userId order by c.option.id asc")
     List<Cart> findByUserIdOrderByOptionIdAsc(int userId);
 
     void deleteByUserId(int userId);
